8764bf1b0b9a09dc92907ae81fd9443121009062
jcanada <j.canada@klab.com>
2016-05-17 15:00:29 +0800

    created test json file

--- AndroidTopicReport1/app/src/main/java/com/example/rikacanada/androidtopicreport1/MainActivityFragment.java
+++ AndroidTopicReport1/app/src/main/java/com/example/rikacanada/androidtopicreport1/MainActivityFragment.java
@@ -1,13 +1,10 @@
 package com.example.rikacanada.androidtopicreport1;
 
-import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridLayout;
 import android.widget.TextView;
 
 import com.android.volley.Response;
@@ -16,8 +13,6 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
-
-import java.io.IOException;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -49,7 +44,6 @@ public class MainActivityFragment extends Fragment {
                     }
                 });
         Volley.newRequestQueue(getActivity()).add(request);
-        //help
         return v;
     }
 }
